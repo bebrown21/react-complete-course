c7600399006554fb4773fb4b2312b5d7c5536d4b
Brandon Brown <Brandon.Brown@metova.com>
2018-12-10 15:56:31 -0600

    add ingredients component

--- burger-builder-app/src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ burger-builder-app/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -4,43 +4,43 @@ import PropTypes from 'prop-types';
 import classes from './BurgerIngredient.css';
 
 class BurgerIngredient extends Component {
-  rendor () {
+  render() {
     let ingredient = null;
 
-    switch(this.props.type) {
-      case ('bread-bottom'):
-        ingredient = <div className={classes.BreadBottom}></div>;
+    switch (this.props.type) {
+      case 'bread-bottom':
+        ingredient = <div className={classes.BreadBottom} />;
         break;
-      case ('bread-top'):
+      case 'bread-top':
         ingredient = (
           <div className={classes.BreadTop}>
-            <div className={classes.Seeds1}></div>
-            <div className={classes.Seeds2}></div>
+            <div className={classes.Seeds1} />
+            <div className={classes.Seeds2} />
           </div>
         );
         break;
-      case ('meat'):
-        ingredient = <div className={classes.Meat}></div>;
+      case 'meat':
+        ingredient = <div className={classes.Meat} />;
         break;
-      case ('cheese'):
-        ingredient = <div className={classes.Cheese}></div>;
+      case 'cheese':
+        ingredient = <div className={classes.Cheese} />;
         break;
-      case ('bacon'):
-        ingredient = <div className={classes.Bacon}></div>;
+      case 'bacon':
+        ingredient = <div className={classes.Bacon} />;
         break;
-      case ('lettuce'):
-        ingredient = <div className={classes.Lettuce}></div>;
+      case 'lettuce':
+        ingredient = <div className={classes.Lettuce} />;
         break;
       default:
         ingredient = null;
     }
-  
+
     return ingredient;
   }
 }
 
 BurgerIngredient.propTypes = {
   type: PropTypes.string.isRequired
-}
+};
 
 export default BurgerIngredient;
