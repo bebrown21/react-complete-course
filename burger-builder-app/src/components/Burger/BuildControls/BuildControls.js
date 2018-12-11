c417aabd2584cf03160957187a74bfc2396d53c0
Brandon Brown <Brandon.Brown@metova.com>
2018-12-10 20:14:34 -0600

    add burger ingredient feature

---
+++ burger-builder-app/src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import BuildControl from './BuildControl/BuildControl';
+
+import classes from './BuildControls.css';
+
+const controls = [
+  { label: 'Lettuce', type: 'lettuce' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Meat', type: 'meat' },
+  { label: 'Cheese', type: 'cheese' },
+];
+
+const buildControls = (props) => (
+  <div className={classes.BuildControls}>
+    {controls.map(control => (
+      <BuildControl 
+        key={control.label} 
+        label={control.label} 
+        added={() => props.ingredientAdded(control.type)}
+        removed={() => props.ingredientRemoved(control.type)} 
+        disabled={props.disabled[control.type]}/>
+    ))}
+  </div>
+);
+
+export default buildControls;
