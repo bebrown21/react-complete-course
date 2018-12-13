dd58f2fe2a48df5b2dece90b781943c38072351e
Brandon Brown <Brandon.Brown@metova.com>
2018-12-13 14:50:17 -0600

    update navigation and toolbar

--- burger-builder-app/src/components/Layout/Layout.js
+++ burger-builder-app/src/components/Layout/Layout.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import Aux from '../../hoc/Aux';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import classes from './Layout.css';
 
-const layout = (props) => (
-  <Aux>
-    <div>Toolbar, SideDrawer, Backdrop</div>
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </Aux>
-);
+class Layout extends Component {
+  state = {
+    displaySideDrawer: false
+  };
 
-export default layout;
+  sideDrawerClosedHandler = () => {
+    this.setState({
+      displaySideDrawer: false
+    });
+  }
+
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return {
+        displaySideDrawer: !prevState.displaySideDrawer
+      }
+    });
+  }
+
+  render () {
+    return (
+      <Aux>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+        <SideDrawer closed={this.sideDrawerClosedHandler} open={this.state.displaySideDrawer}/>
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Aux>
+    );
+  }
+}
+
+export default Layout;
