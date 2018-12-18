177d69f3500bd42208771622143fdd4d7a269b7b
Brandon Brown <Brandon.Brown@metova.com>
2018-12-18 15:40:23 -0600

    add orders and routing

--- burger-builder-app/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ burger-builder-app/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import classes from './NavigationItem.css';
 
 const navigationItem = (props) => (
   <li className={classes.NavigationItem}>
-    <a classNam={props.active ? classes.active : null} href={props.link}>{props.children}</a>
+    <NavLink to={props.link} activeClassName={classes.active} exact={props.exact}>{props.children}</NavLink>
   </li>
 );
 
--- burger-builder-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -51,29 +51,16 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Bob',
-        address: {
-          street: '111 St',
-          zipCode: '812034',
-          country: 'USA'
-        },
-        email: 'test@test.com'
-      },
-      deliveryMethod: 'fast'
-    };
-
-    axios.post('/order.json', order)
-      .then(response => {
-        this.setState({ loading: false, purchasing: false });
-      })
-      .catch(error => {
-        this.setState({ loading: false, purchasing: false });
-      });
+    const queryParams = [];
+    for (let i in this.state.ingredients) {
+      queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+    }
+    queryParams.push('price=' + this.state.totalPrice);
+    const queryString = queryParams.join('&');
+    this.props.history.push({
+      pathname: '/checkout',
+      search: '?' + queryString
+    });
   }
 
   updateIsPurchasableState = (ingredients) => {
